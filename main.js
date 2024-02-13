efd73bb6ba95f17068ef8e8bff91e3679346d049
Min Thiha Kyaw <minthihakyaw32@gmail.com>
2024-02-13 15:50:02 +0630

    updated

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ document.querySelector('#search-icon').onclick =() =>{ /* if you click search-ic
         search.classList.toggle('active'); /* show search-box */
         cart.classList.remove('active'); /* hide cart when search-box open */
         user.classList.remove('active');/* hide user-box when cart open */
+        navbar.classList.remove('active'); /* Hide user-box */
 
     }
 
@@ -13,6 +14,7 @@ document.querySelector('#cart-icon').onclick =() =>{ /* if you click cart-icon *
         cart.classList.toggle('active'); /* show cart-box */
         search.classList.remove('active');/* hide search-box when cart open */
         user.classList.remove('active');/* hide user-box when cart open */
+        navbar.classList.remove('active'); /* Hide user-box */
 
 }
 
@@ -22,10 +24,33 @@ document.querySelector('#user-icon').onclick =() =>{ /* if you click user-icon *
         user.classList.toggle('active'); /* show user-box */
         search.classList.remove('active');/* HIDE user-box when cart open */
         cart.classList.remove('active'); /* HIDE cart-box */
+        navbar.classList.remove('active'); /* Hide user-box */
 
 }
 
+let navbar = document.querySelector('.navbar');
 
+document.querySelector('#menu-icon').onclick =() =>{ /* if you click user-icon */
+        navbar.classList.toggle('active'); /* show user-box */
+        search.classList.remove('active');/* HIDE user-box when cart open */
+        cart.classList.remove('active'); /* HIDE cart-box */
+        user.classList.remove('active'); /* show user-box */
+
+}
+
+window.onscroll = () => {
+        search.classList.remove('active');/* HIDE user-box when cart open */
+        cart.classList.remove('active'); /* HIDE cart-box */
+        user.classList.remove('active'); /* hide user-box */
+        navbar.classList.remove('active'); /* hide user-box */
+}
+
+//  Navbar scroll
+
+let header = document.querySelector('header');
+window.addEventListener("scroll", ()=> {
+        header.classList.toggle('shadow', window.scrollY > 0)
+});
 
 
 // swiper 
@@ -34,7 +59,7 @@ var swiper = new Swiper(".new-arrival", {
         spaceBetween: 20,
         loop: true,
         autoplay: {
-          delay: 3000,
+          delay: 2500,
           disableOnInteraction: false,
         },
         centeredSlides: true,
